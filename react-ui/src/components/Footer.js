0732f4870a7a87dd7fc404c341f44bc303d43b2c
pancho1337 <pancho404x@gmail.com>
2020-01-29 23:07:14 -0600

    working local

---
+++ react-ui/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Footer extends Component{
+    render(){
+        return(
+            <h1>I am the Footer bar</h1>
+        )
+    }
+
+}
+
+export default Footer;
---
+++ react-ui/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component{
+    render(){
+        return(
+            <h1>I am the nav bar</h1>
+        )
+    }
+
+}
+
+export default Header;
